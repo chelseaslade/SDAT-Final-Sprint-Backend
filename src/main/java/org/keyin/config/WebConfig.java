056a5a55c748b3a7f1a794531f1cc11eb9c04326
Chelsea Slade <chelsea.mayne@keyin.com>
2025-04-01 13:43:16 -0230

    Added WebConfig to allow frontend to connect with backend

---
+++ src/main/java/org/keyin/config/WebConfig.java
@@ -0,0 +1,14 @@
+package org.keyin.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+//Allows frontend on localhost3000 to connect with backend
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+    }
+}
